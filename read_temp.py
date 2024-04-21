37c2e4413a9922c6dc950bc3095e0104c5248564
Michael Mühlemann <m.muehlemann@gmx.net>
2024-04-21 18:04:39 +0200

    First Commit

---
+++ read_temp.py
@@ -0,0 +1,33 @@
+#!/usr/.venv/bme280/bin/env python
+
+import time
+import datetime
+
+from smbus2 import SMBus
+
+from bme280 import BME280
+
+print(
+    """all-values.py - Read temperature, pressure, and humidity
+
+Press Ctrl+C to exit!
+
+"""
+)
+
+# Initialise the BME280
+bus = SMBus(1)
+bme280 = BME280(i2c_dev=bus)
+bme280.setup(mode="forced")
+bme280.setup(temperature_oversampling = 1, pressure_oversampling = 1, humidity_oversampling = 1)
+
+
+while True:
+    temperature = bme280.get_temperature()
+    pressure = bme280.get_pressure()
+    humidity = bme280.get_humidity()
+    print(f"{temperature:05.2f}°C {pressure:05.2f}hPa {humidity:05.2f}%")
+    now = datetime.datetime.now()
+    with open("temp_data.csv", 'a') as file:
+        file.write(f"{temperature:05.2f}°C,{pressure:05.2f}hPa,{humidity:05.2f}%,{now.strftime('%d-%m-%Y')}\n")
+    time.sleep(10*60)
